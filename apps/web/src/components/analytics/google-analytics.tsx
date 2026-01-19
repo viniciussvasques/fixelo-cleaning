23d45f632bcfe0c1f38dfc5285ee3cf31ed8c326
Vinicius Vasques <viniciusvasqueslog@gmail.com>
2026-01-19 18:31:45 -0500

    fix: Remove duplicate FacebookPixel from google-analytics.tsx

--- apps/web/src/components/analytics/google-analytics.tsx
+++ apps/web/src/components/analytics/google-analytics.tsx
@@ -73,27 +73,4 @@ export function GoogleTagManagerNoScript() {
     );
 }
 
-// Facebook Pixel
-const FB_PIXEL_ID = '1242796121071698';
-
-export function FacebookPixel() {
-    return (
-        <Script id="facebook-pixel" strategy="afterInteractive">
-            {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        if(!window._fbPixelInitialized){
-          window._fbPixelInitialized=true;
-          fbq('init', '${FB_PIXEL_ID}');
-          fbq('track', 'PageView');
-        }
-      `}
-        </Script>
-    );
-}
+// Note: Facebook Pixel is configured in layout.tsx to avoid duplicates
